180710471fe27fb31fa89d898073dbdfcdafd9e0
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2018-07-24 18:03:26 +0200

    Use multiple files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-'use strict';
+const CreditCardInformation = require('./lib/CreditCardInformation');
+const ExpiryNumber = require('./lib/ExpiryNumber');
+const MII = require('./lib/MII');
+const testNumbers = require('./lib/testNumbers');
 
 class CreditCard {
   /**
@@ -197,131 +200,9 @@ class CreditCard {
   };
 }
 
-CreditCard.CreditCardInformation = class CreditCardInformation {
-  /**
-   * @constructor
-   * @param {object} info Credit card information.
-   * @param {string} iin Issues Identifier Number.
-   * @param {string} mii Major Industry Identifier.
-   * @param {string} formatted Formatted version.
-   * @param {number} cvv
-   * @param {string} truncated PAN truncated version.
-   * @param {string} scheme Credit card scheme.
-   * @param {boolean} valid Credit card validation status.
-   */
-  constructor(info) {
-    this.info = info;
-  }
-}
+CreditCard.CreditCardInformation = CreditCardInformation;
+CreditCard.ExpiryNumber = ExpiryNumber;
+CreditCard.MII = MII;
+CreditCard.testNumbers = testNumbers;
 
-CreditCard.ExpiryNumber = class ExpiryNumber {
-  /**
-   * @constructor
-   * @param {object} expiryNumber
-   * @param {String|Number} expiryNumber.month Expiry month.
-   * @param {String|Number} expiryNumber.year Expiry year.
-   */
-  constructor({ month, year }) {
-    this.month = Number(month);
-    this.year = Number(year);
-  }
-
-  /**
-   * Validates the expiry number.
-   *
-   * @returns {Boolean}
-   * @api public
-   */
-  isValid() {
-    const month = this.month;
-    const year = this.year;
-
-    // incorrect numbers should fail fast instantly
-    if (!month || !year || month > 12) return false;
-
-    const date = new Date();
-
-    date.setFullYear(year);
-    date.setMonth(--month);
-
-    return date.getTime() >= Date.now();
-  }
-}
-
-/**
- * Major Industry Identifier.
- *
- * The first digit of a ISO/IEC 7812 issuer identifier number (inn) tells about
- * what industry the card is used. The index of the array should be the first
- * number of the inn.
- *
- * @type {Array}
- * @public
- */
-CreditCard.MII = [
-  'ISO/TC 68 and other industry assignments',
-  'Airlines',
-  'Airlines and other future industry assignments',
-  'Travel and entertainment and banking/financial',
-  'Banking and financial',
-  'Banking and financial',
-  'Merchandising and banking/financial',
-  'Petroleum and other future industry assignments',
-  'Healthcare, telecommunications and other future industry assignments',
-  'For assignment by national standards bodies'
-];
-
-/**
- * Test numbers from different credit card schemes. Most of them are taken from
- * http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
- *
- * @type {Array}
- * @public
- */
-CreditCard.testNumbers = [
-  4000000000000002, // Recurly test number
-  4000000000000010, // Recurly test number
-  4000000000000028, // Recurly test number
-  4000000000000036, // Recurly test number
-  4000000000000044, // Recurly test number
-  4000000000000051, // Recurly test number
-  4000000000000077, // Recurly test number
-  4000000000000085, // Recurly test number
-  4000000000000093, // Recurly test number
-  4000000000000101, // Recurly test number
-  4000000000000119, // Recurly test number
-  4000000000000200, // Recurly test number
-  4222222222222222, // Recurly test number
-  4000000000000226, // Recurly test number
-  4000000000000309, // Recurly test number
-  4000000000000317, // Recurly test number
-  4000000000000325, // Recurly test number
-  4000000000000341, // Recurly test number
-  4222222222222, // visa
-  4012888888881881, // visa
-  4111111111111111, // visa
-  5105105105105100, // mastercard
-  5555555555554444, // mastercard
-  3566002020360505, // jbc
-  3530111333300000, // jbc
-  6011000990139424, // discover
-  6011111111111117, // discover
-  6011601160116611, // discover
-  38520000023237, // diners club
-  30569309025904, // diners club
-  378734493671000, // american express
-  371449635398431, // american express
-  378282246310005, // american express
-  341111111111111, // american express
-  5431111111111111, // mastercard
-  5610591081018250, // australian bank
-  5019717010103742, // dankort (pbs)
-  76009244561, // dankort (pbs)
-  6331101999990016 // switch/solo (paymentech)
-];
-
-if (module && module.exports) {
-  module.exports = CreditCard;
-} else {
-  global.CreditCard = CreditCard;
-}
+module.exports = CreditCard;
